517b5e874e64c90cc37b7154ecfc9fd026160711
Sérgio Alonso Jr <SergioAlonsoJunior94@gmail.com>
2019-11-22 10:15:26 -0300

    Torna gerador a página principal

--- gerador_clipping/gerador_clipping/urls.py
+++ gerador_clipping/gerador_clipping/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('gerador/', include('gerador.urls')),
+    path('', include('gerador.urls')),
     path('admin/', admin.site.urls),
 ]
