fc55fb5ce5a1d667d6757d339e6bbd6618afda5a
Sérgio Alonso Jr <SergioAlonsoJunior94@gmail.com>
2019-11-26 20:14:40 -0300

    Muda DB de SQLite para postgreSQL.

--- gerador_clipping/gerador/models.py
+++ gerador_clipping/gerador/models.py
@@ -19,14 +19,14 @@ class News(models.Model):
     """" News é uma notícia do clipping. """
     project = models.ForeignKey(ClippingProject, on_delete=models.CASCADE)
     order = models.PositiveSmallIntegerField(default=0)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=500)
     content = models.CharField(max_length=500)
-    url = models.CharField(max_length=300)
+    url = models.CharField(max_length=500)
     pub_date = models.DateTimeField('published date')
-    author = models.CharField(max_length=100, default='0')
-    url_to_image = models.CharField(max_length=100, default='0')
-    source_db_id = models.CharField(max_length=300, default='0')
-    header = models.CharField(max_length=50, default="")
+    author = models.CharField(max_length=500, default='0')
+    url_to_image = models.CharField(max_length=500, default='0')
+    source_db_id = models.CharField(max_length=500, default='0')
+    header = models.CharField(max_length=500, default="")
 
     def __str__(self):
         return self.title
