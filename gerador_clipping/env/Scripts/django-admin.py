432ee9f85264bfaa6fe9303094ea9208ffe3fa4d
Sérgio Alonso Jr <SergioAlonsoJunior94@gmail.com>
2019-12-03 10:39:53 -0300

    Elimina .gitignore

---
+++ gerador_clipping/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\unb\si\gerador_clipping_20192\gerador_clipping\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
